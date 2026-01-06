2a9119e268bb3a207fefbb251b9709bf32c19847
Zeus-newui <davedelossantos1027@gmail.com>
2026-01-06 16:07:32 +0800

    Update main.lua

--- keysystem/main.lua
+++ keysystem/main.lua
@@ -1,9 +1,12 @@
 -- Create a function to load scripts based on the game
+repeat wait(1) until game:IsLoaded()
 local function loadGameScript()
     local placeId = game.PlaceId
 
     if placeId == 17625359962 then -- Rivals
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeus-newui/Junkie-Development/refs/heads/main/keysystem/games/rivals.lua"))()
+elseif PlaceId == 7671049560 then -- The Forge 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Zeus-newui/Junkie-Development/refs/heads/main/keysystem/games/theforge.lua"))()
     else
         print("This script is not applicable for the current game.")
     end
