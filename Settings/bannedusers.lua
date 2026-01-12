be860575cfa7e11f26eb691214b9208b5ca5db8f
Zeus-newui <davedelossantos1027@gmail.com>
2026-01-12 19:03:00 +0800

    Update bannedusers.lua

--- Settings/bannedusers.lua
+++ Settings/bannedusers.lua
@@ -1,1 +1,17 @@
+-- This is a display-only banned users list.
+-- The following User IDs are banned:
+
+--[[
+return { 
+  2526634520, -- kierbermio
+  9926088572, -- Zaru0912
+  3573965288, -- arisgado3
+  10134729333 -- oneshotonekill1027
+}
+--]]
+
+return { 
+  -- No banned users at this time
+}
+
 
