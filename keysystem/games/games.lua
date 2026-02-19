65db3c81b70824a4521bc6815d3c58f1a49c6538
Zeus-newui <davedelossantos1027@gmail.com>
2026-02-19 13:42:23 +0800

    Create games.lua

---
+++ keysystem/games/games.lua
@@ -0,0 +1,5 @@
+return {
+    [9363735110] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Escape%20Tsunami%20for%20Brainrot.lua",
+    [9497625867] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Escape%20logs%20for%20Brainrot.lua",
+    [9344307274] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Break%20a%20Lucky%20Block.lua",
+}
