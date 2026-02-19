46303be676c2761a8ea65de123444d37918e1014
Zeus-newui <davedelossantos1027@gmail.com>
2026-02-19 13:45:59 +0800

    Update games.lua

--- keysystem/games/games.lua
+++ keysystem/games/games.lua
@@ -1,5 +1,9 @@
-return {
-    [9363735110] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Escape%20Tsunami%20for%20Brainrot.lua",
-    [9497625867] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Escape%20logs%20for%20Brainrot.lua",
-    [9344307274] = "https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Break%20a%20Lucky%20Block.lua",
-}
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Games.lua"))()
+local scriptUrl = Games[game.GameId]
+if scriptUrl then
+    loadstring(game:HttpGet(scriptUrl))()
+    task.spawn(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Ratkinator/RatX/refs/heads/main/Executed.lua"))() end)
+else
+    warn("❌ Unsupported game | GameId:", game.GameId)
+end
+return Games
